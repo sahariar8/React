d9aa6495f89be4d13caee9397fe147e0b92d6fe6
Sahariar <sahariaralam8@gmail.com>
2024-02-04 21:15:37 +0600

    from feature2

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <>
        <h1 className='text-5xl text-yellow-600 font-bold text-center'>My name is Shahariar</h1>
+       <h2 className='text-2xl text-red-500 font-semibold text-center mt-5'>I AM ADDING FROM FEATURE 2 BRANCH</h2>
     </>
   )
 }
